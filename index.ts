3410d10ab79a462208677d01caed468ef0f22cb8
Ben <manglebot@users.noreply.github.com>
2023-11-06 09:38:50 +0000

    feat:add package.json and index.ts and gitignore

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { missionControl } from './src/mission-control';
+// import { clear, print, askQuestion } from './src/ui/console';
+
+export function moveRover(test):string {
+	return "00N";
+}
